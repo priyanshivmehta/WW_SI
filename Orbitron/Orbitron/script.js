5c59a34396e7cd1e4ef23c7df5dfa07bd37ee61f
priyanshivmehta <pvmehta112@gmail.com>
2024-07-17 00:53:35 +0530

    Update script.js

--- Orbitron/Orbitron/script.js
+++ Orbitron/Orbitron/script.js
@@ -18,271 +18,182 @@ function myFunction() {
 function showPage() {
   document.getElementById("myDiv").style.display = "block";
 }
-let questions = [
-  {
-      prompt: `Inside which HTML 
-               element do we put 
-               the JavaScript?`,
-      options: [
-          "<javascript>",
-          "<js>",
-          "<script>",
-          "<scripting>",
-      ],
-      answer: "<script>",
-  },
 
+// QUIZ
+const questions=[
   {
-      prompt: `How do you call a
-               function named 
-               myFunction?`,
-      options: [
-          "call myFunction()",
-          "myFunction()",
-          "call function myFunction",
-          "Call.myFunction",
-      ],
-      answer: "myFunction()",
+    question: "What is the escape velocity from Earth's surface?",
+    answers: [
+      { text: "9.8 km/s", correct: false},
+      { text: "11.2 km/s", correct: true},
+      { text: "16.5 km/s", correct: false},
+      { text: "25.2 km/s", correct: false},
+    ]
   },
-
   {
-      prompt: `How does a for loop
-               start?`,
-      options: [
-          "for (i = 0; i <= 5; i++)",
-          "for (i = 0; i <= 5)",
-          "for i = 1 to 5",
-          " for (i <= 5; i++)",
-      ],
-      answer: "for (i = 0; i <= 5; i++)",
+    question: "Which planet has the shortest day in our solar system?",
+    answers: [
+      { text: "Jupiter", correct: true},
+      { text: "Mars", correct: false},
+      { text: "Neptune", correct: false},
+      { text: "Mercury", correct: false},
+    ]
   },
-
   {
-      prompt: `In JavaScript, which 
-               of the following is 
-               a logical operator?`,
-      options: ["|", "&&", "%", "/"],
-      answer: "&&",
+    question: "What is the name of the boundary around a black hole beyond which no light can escape?",
+    answers: [
+      { text: "Event Horizon", correct: true},
+      { text: "Singularity", correct: false},
+      { text: "Schwarzschild Radius", correct: false},
+      { text: "Accretion Disk", correct: false},
+    ]
   },
-
   {
-      prompt: `A named element in a 
-               JavaScript program that
-               is used to store and 
-               retrieve data is a _____.`,
-      options: [
-          "method",
-          "assignment operator",
-          "letiable",
-          "string",
-      ],
-      answer: "letiable",
+    question: " Which spacecraft was the first to reach interstellar space?",
+    answers: [
+      { text: "Pioneer 10", correct: false},
+      { text: "Voyager 1", correct: true},
+      { text: "Voyager 2", correct: false},
+      { text: "New Horizons", correct: false},
+    ]
   },
+  {
+    question: "What is the approximate age of the universe?",
+    answers: [
+      { text: "4.5 billion years", correct: false},
+      { text: "10 billion years", correct: false},
+      { text: "13.8 billion years", correct: true},
+      { text: "20 billion years", correct: false},
+    ]
+  },
+  {
+    question: "Which planet has the tallest volcano in the solar system, and what is its name?",
+    answers: [
+      { text: "Mars, Olympus Mons", correct: true},
+      { text: "Venus, Maxwell Montes", correct: false},
+      { text: "Earth, Mauna Kea", correct: false},
+      { text: "Mercury, Caloris Montes", correct: false},
+    ]
+  },
+  {
+    question: "What phenomenon causes the tails of comets to always point away from the sun?",
+    answers: [
+      { text: "Solar Wind", correct: true},
+      { text: "Gravitational Pull", correct: false},
+      { text: "Photon Pressure", correct: false},
+      { text: "Magnetic Fields", correct: false},
+    ]
+  },
+  {
+    question: "What is the largest type of star in the universe?",
+    answers: [
+      { text: "Red Dwarf", correct: false},
+      { text: "Blue Giant", correct: false},
+      { text: "Red Supergiant", correct: true},
+      { text: "White Dwarf", correct: false},
+    ]
+  },
+  {
+    question: "What is the name of the effect that causes light from distant galaxies to appear redshifted?",
+    answers: [
+      { text: "Doppler Effect", correct: false},
+      { text: "Gravitatiional Lensing", correct: false},
+      { text: "Cosmic Inflation", correct: false},
+      { text: "Hubble Effect", correct: true},
+    ]
+  },
+  {
+    question: "What is the term for the hypothetical point in time when the universe's expansion will reverse, leading to a 'Big Crunch'?",
+    answers: [
+      { text: "Big Bang", correct: false},
+      { text: "Heat Death", correct: false},
+      { text: "Big Freeze", correct: false},
+      { text: "Big Crunch", correct: true},
+    ]
+  }
 ];
 
-// Get Dom Elements
+const questionelement=document.getElementById("question");
+const answerbuttons=document.getElementById("answer-buttons");
+const nextbutton=document.getElementById("next-btn");
 
-let questionsEl =
-  document.querySelector(
-      "#questions"
-  );
-let timerEl =
-  document.querySelector("#timer");
-let choicesEl =
-  document.querySelector("#options");
-let submitBtn = document.querySelector(
-  "#submit-score"
-);
-let startBtn =
-  document.querySelector("#start");
-let nameEl =
-  document.querySelector("#name");
-let feedbackEl = document.querySelector(
-  "#feedback"
-);
-let reStartBtn =
-  document.querySelector("#restart");
+let currentquestionindex=0;
+let score=0;
 
-// Quiz's initial state
-let currentQuestionIndex = 0;
-let time = questions.length * 15;
-let timerId;
-
-// Start quiz and hide frontpage
-
-function quizStart() {
-  timerId = setInterval(
-      clockTick,
-      1000
-  );
-  timerEl.textContent = time;
-  let landingScreenEl =
-      document.getElementById(
-          "start-screen"
-      );
-  landingScreenEl.setAttribute(
-      "class",
-      "hide"
-  );
-  questionsEl.removeAttribute(
-      "class"
-  );
-  getQuestion();
+function StartQuiz(){
+  currentquestionindex=0;
+  score=0;
+  nextbutton.innerHTML="Next";
+  showQuestion();
 }
 
-// Loop through array of questions and
-// Answers and create list with buttons
-function getQuestion() {
-  let currentQuestion =
-      questions[currentQuestionIndex];
-  let promptEl =
-      document.getElementById(
-          "question-words"
-      );
-  promptEl.textContent =
-      currentQuestion.prompt;
-  choicesEl.innerHTML = "";
-  currentQuestion.options.forEach(
-      function (choice, i) {
-          let choiceBtn =
-              document.createElement(
-                  "button"
-              );
-          choiceBtn.setAttribute(
-              "value",
-              choice
-          );
-          choiceBtn.textContent =
-              i + 1 + ". " + choice;
-          choiceBtn.onclick =
-              questionClick;
-          choicesEl.appendChild(
-              choiceBtn
-          );
-      }
-  );
+function showQuestion(){
+  resetstate();
+  let currentquestion=questions[currentquestionindex];
+  let questionno = currentquestionindex + 1;
+  questionelement.innerHTML=questionno + ". "+ currentquestion.
+  question;
+
+  currentquestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML=answer.text;
+    button.classList.add("btn");
+    answerbuttons.appendChild(button);
+    if(answer.correct){
+      button.dataset.correct=answer.correct;
+    }
+    button.addEventListener("click",selectanswer);
+  }); 
 }
 
-// Check for right answers and deduct
-// Time for wrong answer, go to next question
-
-function questionClick() {
-  if (
-      this.value !==
-      questions[currentQuestionIndex]
-          .answer
-  ) {
-      time -= 10;
-      if (time < 0) {
-          time = 0;
-      }
-      timerEl.textContent = time;
-      feedbackEl.textContent = `Wrong! The correct answer was 
-      ${questions[currentQuestionIndex].answer}.`;
-      feedbackEl.style.color = "red";
-  } else {
-      feedbackEl.textContent =
-          "Correct!";
-      feedbackEl.style.color =
-          "green";
-  }
-  feedbackEl.setAttribute(
-      "class",
-      "feedback"
-  );
-  setTimeout(function () {
-      feedbackEl.setAttribute(
-          "class",
-          "feedback hide"
-      );
-  }, 2000);
-  currentQuestionIndex++;
-  if (
-      currentQuestionIndex ===
-      questions.length
-  ) {
-      quizEnd();
-  } else {
-      getQuestion();
+function resetstate(){
+  nextbutton.style.display="none";
+  while(answerbuttons.firstChild){
+    answerbuttons.removeChild(answerbuttons.firstChild);
   }
 }
 
-// End quiz by hiding questions,
-// Stop timer and show final score
-
-function quizEnd() {
-  clearInterval(timerId);
-  let endScreenEl =
-      document.getElementById(
-          "quiz-end"
-      );
-  endScreenEl.removeAttribute(
-      "class"
-  );
-  let finalScoreEl =
-      document.getElementById(
-          "score-final"
-      );
-  finalScoreEl.textContent = time;
-  questionsEl.setAttribute(
-      "class",
-      "hide"
-  );
+function selectanswer(e){
+  const selectedbtn=e.target;
+  const iscorrect=selectedbtn.dataset.correct ==="true";
+  if(iscorrect){
+    selectedbtn.classList.add("correct");
+    score++;
+  }else{
+    selectedbtn.classList.add("incorrect");
+  }
+  Array.from(answerbuttons.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+      button.classList.add("correct");
+    }
+    button.disabled=true;
+  });
+  nextbutton.style.display="block";
 }
 
-// End quiz if timer reaches 0
+function showscore(){
+  resetstate();
+  questionelement.innerHTML= `Congratulations, You scored ${score} out of ${questions.
+  length}!`;
+  nextbutton.innerHTML="Play Again";
+  nextbutton.style.display="block";
+}
 
-function clockTick() {
-  time--;
-  timerEl.textContent = time;
-  if (time <= 0) {
-      quizEnd();
+function handlenextbutton(){
+  currentquestionindex++;
+  if(currentquestionindex < questions.length){
+    showQuestion();
+  }else{
+    showscore();
   }
 }
 
-// Save score in local storage
-// Along with users' name
-
-function saveHighscore() {
-  let name = nameEl.value.trim();
-  if (name !== "") {
-      let highscores =
-          JSON.parse(
-              window.localStorage.getItem(
-                  "highscores"
-              )
-          ) || [];
-      let newScore = {
-          score: time,
-          name: name,
-      };
-      highscores.push(newScore);
-      window.localStorage.setItem(
-          "highscores",
-          JSON.stringify(highscores)
-      );
-      alert(
-          "Your Score has been Submitted"
-      );
+nextbutton.addEventListener("click", ()=>{
+  if(currentquestionindex < questions.length){
+    handlenextbutton();
+  }else{
+    StartQuiz();
   }
-}
+})
 
-// Save users' score after pressing enter
-
-function checkForEnter(event) {
-  if (event.key === "Enter") {
-      saveHighscore();
-      alert(
-          "Your Score has been Submitted"
-      );
-  }
-}
-nameEl.onkeyup = checkForEnter;
-
-// Save users' score after clicking submit
-
-submitBtn.onclick = saveHighscore;
-
-// Start quiz after clicking start quiz
-
-startBtn.onclick = quizStart;
+StartQuiz();
