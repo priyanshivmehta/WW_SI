ec01aa2a2c10c362e27589f2cd0945d5ed8a2217
MIKU-intheverse <chawdamamta64@gmail.com>
2024-07-24 00:41:59 +0530

    Add files via upload

---
+++ cosmos/new.js
@@ -0,0 +1,21 @@
+window.onscroll = () => {
+
+    document.querySelector(".bg").style.marginTop = scrollY / 2 + "px";
+
+    document.querySelector(".welcome").style.marginTop = scrollY / 2 + "px";
+
+    document.querySelector(".mountains").style.transform = "scale(" + (scrollY / 3000 + 1) + ")";
+}
+
+const toggleBtn= document.querySelector('.toggle_btn');
+const toggleBtnIcon= document.querySelector('.toggle_btn i');
+const dropDownMenu= document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick= function(){
+    dropDownMenu.classList.toggle('open')
+    const isOpen=dropDownMenu.classList.contains('open')
+    toggleBtnIcon.classList=isOpen
+    ?'fa-solid fa-xmark'
+    :'fa-solid fa-bars'
+}
+
